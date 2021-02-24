34fa3f36ccd82dfc3a994b873e428a2cc8dba3e9
jai-jk <jay.jk@hotmail.com>
2021-02-24 12:13:43 +0000

    Fix failing feature test

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -7,9 +7,9 @@ describe Bookmark do
     it 'return a list of bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      connection.exec("INSERT INTO bookmarks VALUES (1, 'http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks VALUES (2, 'http://www.twitter.com');")
-      connection.exec("INSERT INTO bookmarks VALUES (3, 'http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.twitter.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
 
       bookmarks = Bookmark.all
       expect(bookmarks).to include('http://www.makersacademy.com')
